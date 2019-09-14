854cdf572c4f267524c1be79af86eed5e394db52
Hameedshahulh <hameed15shahulh@gmail.com>
2019-09-14 16:15:00 +0530

    Added Employee details

---
+++ src/org/student/TestOne.java
@@ -0,0 +1,21 @@
+package org.student;
+
+public class TestOne
+	{
+	static int a= 10;
+		public static void name()
+		{
+			int b =10;
+		System.out.println("name");
+		System.out.println(b);
+		System.out.println(TestOne.a);
+		}
+		public void number()
+		{
+			System.out.print("number");
+		}
+		public static void main (String [] args)
+		{
+		name();
+		}
+		}
